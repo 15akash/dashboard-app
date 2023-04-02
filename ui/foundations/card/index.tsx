a73b43239daf20fde19d54e742e92040ed049f42
15akash <ajindal1512@gmail.com>
2023-04-03 02:46:29 +0530

    fixing build issue

--- ui/foundations/card/index.tsx
+++ ui/foundations/card/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import styles from './card.module.scss';
+import styles from './Card.module.scss';
 import Flexbox from '../flexbox';
 
 interface CardProps {
